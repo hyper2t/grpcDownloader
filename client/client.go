9ff4ebd0f07a3ee272ab26a51ab02b7e1bca7dcc
hyper2t <1244240338@qq.com>
2022-09-18 01:15:07 +0800

    remove comment

--- client/client.go
+++ client/client.go
@@ -22,29 +22,4 @@ func Start() {
 	if err := Download(client, context.Background(), fromInfo, toInfo, &fi.Downloader{}); err != nil {
 		log.Fatalf("client failed: %v", err)
 	}
-	//in := &pb.Download{Filepath: "hi peter"}
-	//stream, err := client.Download(context.Background(), in)
-	//if err != nil {
-	//	log.Fatalf("openn stream error %v", err)
-	//}
-	//
-	////ctx := stream.Context()
-	//done := make(chan bool)
-	//
-	//go func() {
-	//	for {
-	//		resp, err := stream.Recv()
-	//		if err == io.EOF {
-	//			done <- true //close(done)
-	//			return
-	//		}
-	//		if err != nil {
-	//			log.Fatalf("can not receive %v", err)
-	//		}
-	//		log.Printf("Resp received: %s", string(resp.GetData()))
-	//	}
-	//}()
-	//
-	//<-done
-	//log.Printf("finished")
 }
